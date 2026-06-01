2e8ded854adf7e8ab89a88b80055ce43f8492a8d
Vonjihery Fabiolah <andriatahinafabiolah@gmail.com>
2026-06-01 15:16:04 +0200

    Fix useEffect dependency

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function useVisible(ref) {
     const o = new IntersectionObserver(([e]) => { if (e.isIntersecting) setV(true); }, { threshold: 0.1 });
     if (ref.current) o.observe(ref.current);
     return () => o.disconnect();
-  }, []);
+  }, [ref]);
   return v;
 }
 
